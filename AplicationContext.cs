ae461a50badbadff86c3df98e3f3978ec6f07d88
Rodrigo Silva <rodrigocsilva63@gmail.com>
2020-12-23 22:20:59 -0300

    itroducing entity Framework

---
+++ AplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.wwwroot
+{
+    public class AplicationContext : DbContext
+    {
+        public AplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(J => J.Id);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ecommerce.wwwroot;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,12 @@ namespace Ecommerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string ConnectionString = Configuration.GetConnectionString("Default");
+            
+            services.AddDbContext<AplicationContext>(options =>
+                options.UseSqlServer(ConnectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
