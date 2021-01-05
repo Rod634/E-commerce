ec8dbc748bd50bad0748dc669b4fa4b45cb0b035
Rodrigo Silva <rodrigocsilva63@gmail.com>
2021-01-05 08:14:11 -0300

    Adding validation to the database insertion

--- Repositories/ProdutoRepository.cs
+++ Repositories/ProdutoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,22 +8,27 @@ namespace Ecommerce.Repositories
     public class ProdutoRepository : IProdutoRepository
     {
         private readonly AplicationContext contexto;
+        private readonly DbSet<Produto> dbset;
 
         public ProdutoRepository(AplicationContext contexto)
         {
             this.contexto = contexto;
+            dbset = contexto.Set<Produto>();
         }
 
         public IList<Produto> GetProdutos()
         {
-            return contexto.Set<Produto>().ToList();
+            return dbset.ToList();
         }
 
         public void SalvarMudancas(List<Livro> livros)
         {
             foreach (var livro in livros)
             {
-                contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                if(!dbset.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    dbset.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                }
             }
 
             contexto.SaveChanges();
