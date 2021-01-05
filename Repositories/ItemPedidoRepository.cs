4e007f2e1e703745f01316b61f6a7435f6d44b89
Rodrigo Silva <rodrigocsilva63@gmail.com>
2021-01-05 08:27:03 -0300

    Creation of Repository classes

---
+++ Repositories/ItemPedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repositories
+{
+    public class ItemPedidoRepository : BaseRepository<ItemPedido>
+    {
+        public ItemPedidoRepository(AplicationContext contexto) : base(contexto)
+        {
+
+        }
+    }
+}
---
+++ Repositories/PedidoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repositories
+{
+    public class PedidoRepository : BaseRepository<Pedido>
+    {
+        public PedidoRepository(AplicationContext contexto) : base(contexto)
+        {
+        }
+    }
+}
